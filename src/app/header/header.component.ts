f1fb8a8effeada9a3c9184d817d054084618f7a1
joangnz <joangnz@gmail.com>
2025-02-12 19:30:27 +0100

    Fixed: Imported CommonModule to control hover event and class manipulation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { GLOBALS } from '../app.config';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
