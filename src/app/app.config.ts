0007d2f4e8595d5acf6e81c85c919856e0dda887
joangnz <joangnz@gmail.com>
2025-02-12 19:17:09 +0100

    Format?

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,19 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+  ],
 };
 
 export const GLOBALS = {
-  SITE_URL: "http://localhost:4200",
-}
+  SITE_URL: 'http://localhost:4200',
+};
