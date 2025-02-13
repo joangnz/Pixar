ab8ba87f3f065694be9d84837df914e7dd0b9eb1
joangnz <joangnz@gmail.com>
2025-02-13 18:57:13 +0100

    Implement: Globals

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { GLOBALS } from '../app.config';
 
 @Component({
   selector: 'app-hero',
   imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
 export class HeroComponent {
-
+  SITE_URL = GLOBALS.SITE_URL;
 }
