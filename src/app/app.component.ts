63e469314a6af02f58ea1281536bbdece397df18
joangnz <joangnz@gmail.com>
2025-02-13 17:49:49 +0100

    Creation and implementation: Hero Component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
 import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
