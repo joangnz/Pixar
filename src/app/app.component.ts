ad2cddedb2af5e22b2b2a999c351480209bf2419
joangnz <joangnz@gmail.com>
2025-02-14 16:35:09 +0100

    Creation of Main Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeroComponent } from './hero/hero.component';
+import { MainComponent } from './main/main.component';
 import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, HeroComponent],
+  imports: [HeaderComponent, FooterComponent, HeroComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
