8112649750438ca83f2c00ca675c33daa7475f16
joangnz <joangnz@gmail.com>
2025-02-17 18:23:03 +0100

    Updating main component.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { GLOBALS } from '../app.config';
 
 @Component({
   selector: 'app-main',
   imports: [],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
 export class MainComponent {
-
+  SITE_URL = GLOBALS.SITE_URL;
 }
